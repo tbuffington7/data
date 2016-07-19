bf3a1e4e31c2b5389f32bb998985e34f5dae6798
Tyler Garner <garnertb@gmail.com>
2016-07-19 08:44:44 -0400

    Update the tract_years query.

--- sources/nfirs/scripts/materialized_views/nist.tract_years.sql
+++ sources/nfirs/scripts/materialized_views/nist.tract_years.sql
@@ -3,12 +3,16 @@
 --  number of tables referenced.
 
 CREATE MATERIALIZED VIEW nist.tract_years AS
-(select state, fdid,
-  '14000US' || SUBSTRING(bkgpidfp00, 0, 11) AS tr00_fid,
-  '14000US' || SUBSTRING(bkgpidfp10, 0, 11) as tr10_fid,
-  fd.id as fc_dept_id,
-  extract('year' from inc_date) as year, fd.region
-from incidentaddress ia left join firestation_firedepartment fd using (state, fdid)
-where bkgpidfp00 is not null
-group by state, fdid, bkgpidfp00,bkgpidfp10, fc_dept_id, year, fd.region);
+(WITH t as ( SELECT generate_series( 2000, 2015 ) as year )
+
+SELECT DISTINCT
+    ia.state,
+    fdid,
+    '14000US'::text || "substring"(ia.bkgpidfp00::text, 0, 11) AS tr00_fid,
+    '14000US'::text || "substring"(ia.bkgpidfp10::text, 0, 11) AS tr10_fid,
+    fd.id AS fc_dept_id,
+    fd.region,
+    year
+FROM incidentaddress ia LEFT JOIN firestation_firedepartment fd USING (state, fdid), t
+WHERE ia.bkgpidfp00 IS NOT NULL);
 
