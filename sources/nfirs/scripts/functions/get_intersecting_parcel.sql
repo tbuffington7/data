422cec006ff36262e27aa65d2f00949c01f4838a
Tyler Garner <garnertb@gmail.com>
2017-01-30 10:37:57 -0500

    Use bbox intersection.

--- sources/nfirs/scripts/functions/get_intersecting_parcel.sql
+++ sources/nfirs/scripts/functions/get_intersecting_parcel.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION get_intersecting_parcel(geometry)
 RETURNS integer AS $$
  SELECT parcels.parcel_id::int
       FROM parcel_risk_category_local parcels
-      WHERE ST_Intersects($1, parcels.wkb_geometry)
-       ORDER BY st_distance(st_centroid($1), parcels.wkb_geometry)
+      WHERE $1 && parcels.wkb_geometry
+       ORDER BY st_distance(st_centroid(parcels.wkb_geometry), $1)
       limit 1
 $$ LANGUAGE SQL;
