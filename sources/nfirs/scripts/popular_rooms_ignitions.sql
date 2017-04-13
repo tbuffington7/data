f799e0408febcccfc5eaf9000bbeb2b9479fe205
Joe Meilinger <joemeilinger@gmail.com>
2017-04-13 08:58:33 -0500

    Add in grouped property usages

--- sources/nfirs/scripts/popular_rooms_ignitions.sql
+++ sources/nfirs/scripts/popular_rooms_ignitions.sql
@@ -34,3 +34,35 @@ select area_orig, first_ign, count(1)
 from buildingfires
 group by area_orig, first_ign
 order by count desc
+
+-- For grouped propety usage
+
+SELECT area_orig,
+       first_ign,
+       x.cnt
+FROM
+    ( SELECT *,
+             row_number() over (partition BY area_orig
+                                ORDER BY area_orig, w.cnt DESC, first_ign) row_num
+     FROM
+         (SELECT bf.area_orig,
+                 bf.first_ign,
+                 count(*) OVER ( PARTITION BY bf.area_orig, bf.first_ign ) AS cnt,
+                 row_number() OVER ( PARTITION BY bf.area_orig, bf.first_ign ) AS row_numbers
+          FROM buildingfires bf
+          WHERE bf.area_orig IN
+                  ( SELECT area_orig
+                   FROM buildingfires
+                   WHERE prop_use in ('120', '121', '122', '123', '124', '129')
+                       AND area_orig != 'UU'
+                   GROUP BY area_orig
+                   ORDER BY count(1) DESC LIMIT 8)
+              AND bf.prop_use in ('120', '121', '122', '123', '124', '129')
+              AND bf.first_ign != 'UU'
+          ORDER BY area_orig,
+                   first_ign ) w
+     WHERE w.row_numbers = 1) x
+WHERE x.row_num < 7
+ORDER BY area_orig,
+         x.cnt DESC,
+         first_ign
