e541a18f509227fa0aed6e5318be1455a2308de3
Stanley Gilbert <stanley.gilbert@nist.gov>
2018-08-21 08:33:15 -0400

    fix ACS references

--- sources/nfirs/scripts/materialized_views/nist.med_risk_fires.sql
+++ sources/nfirs/scripts/materialized_views/nist.med_risk_fires.sql
@@ -33,7 +33,7 @@ CREATE MATERIALIZED VIEW nist.med_risk_fires AS
                     WHEN cf.struc = 'Y'::text AND cf.risk = 'Med Risk'::text AND cf.geoid IS NOT NULL THEN cf.ff_death + cf.oth_death
                     ELSE 0
                 END) AS deaths
-           FROM nist.coded_fires cf
+           FROM nist.coded_fires2 cf
           WHERE cf.year > 2006::double precision AND cf.version = 5.0 AND NOT (cf.inc_type::text = '112'::text AND cf.year > 2007::double precision)
           GROUP BY cf.year, cf.geoid
         ), d AS (
@@ -84,39 +84,39 @@ CREATE MATERIALIZED VIEW nist.med_risk_fires AS
     c.injuries,
     c.deaths,
         CASE
-            WHEN acs."B25002_002E" > 0 THEN acs."B01001_001E"::double precision / acs."B25002_002E"::double precision
-            WHEN acs."B25002_002E" = 0 AND acs."B01001_001E" > 0 THEN 'Infinity'::double precision
+            WHEN acs."B25002_002" > 0 THEN acs."B01001_001"::double precision / acs."B25002_002"::double precision
+            WHEN acs."B25002_002" = 0 AND acs."B01001_001" > 0 THEN 'Infinity'::double precision
             ELSE 'NaN'::double precision
         END AS ave_hh_sz,
-    acs."B01001_001E" AS pop,
-    acs."B02001_003E" AS black,
-    acs."B02001_004E" AS amer_es,
-    acs."B02001_005E" + acs."B02001_006E" + acs."B02001_007E" + acs."B02001_008E" AS other,
-    acs."B03003_003E" AS hispanic,
-    acs."B01001_002E" AS males,
-    acs."B01001_003E" + acs."B01001_027E" AS age_under5,
-    acs."B01001_004E" + acs."B01001_028E" AS age_5_9,
-    acs."B01001_005E" + acs."B01001_029E" AS age_10_14,
-    acs."B01001_006E" + acs."B01001_007E" + acs."B01001_030E" + acs."B01001_031E" AS age_15_19,
-    acs."B01001_008E" + acs."B01001_009E" + acs."B01001_010E" + acs."B01001_032E" + acs."B01001_033E" + acs."B01001_034E" AS age_20_24,
-    acs."B01001_011E" + acs."B01001_012E" + acs."B01001_035E" + acs."B01001_036E" AS age_25_34,
-    acs."B01001_013E" + acs."B01001_014E" + acs."B01001_037E" + acs."B01001_038E" AS age_35_44,
-    acs."B01001_015E" + acs."B01001_016E" + acs."B01001_039E" + acs."B01001_040E" AS age_45_54,
-    acs."B01001_017E" + acs."B01001_018E" + acs."B01001_019E" + acs."B01001_041E" + acs."B01001_042E" + acs."B01001_043E" AS age_55_64,
-    acs."B01001_020E" + acs."B01001_021E" + acs."B01001_022E" + acs."B01001_044E" + acs."B01001_045E" + acs."B01001_046E" AS age_65_74,
-    acs."B01001_023E" + acs."B01001_024E" + acs."B01001_047E" + acs."B01001_048E" AS age_75_84,
-    acs."B01001_025E" + acs."B01001_049E" AS age_85_up,
-    acs."B25002_001E" AS hse_units,
-    acs."B25002_003E" AS vacant,
-    acs."B25014_008E" AS renter_occ,
-    acs."B25014_005E" + acs."B25014_006E" + acs."B25014_007E" + acs."B25014_011E" + acs."B25014_012E" + acs."B25014_013E" AS crowded,
-    acs."B25024_002E" + acs."B25024_003E" + acs."B25024_004E" AS sfr,
-    acs."B25024_007E" + acs."B25024_008E" + acs."B25024_009E" AS units_10,
-    acs."B25024_010E" AS mh,
-    acs."B25034_006E" + acs."B25034_007E" + acs."B25034_008E" + acs."B25034_009E" + acs."B25034_010E" AS older,
+    acs."B01001_001" AS pop,
+    acs."B02001_003" AS black,
+    acs."B02001_004" AS amer_es,
+    acs."B02001_005" + acs."B02001_006" + acs."B02001_007" + acs."B02001_008" AS other,
+    acs."B03003_003" AS hispanic,
+    acs."B01001_002" AS males,
+    acs."B01001_003" + acs."B01001_027" AS age_under5,
+    acs."B01001_004" + acs."B01001_028" AS age_5_9,
+    acs."B01001_005" + acs."B01001_029" AS age_10_14,
+    acs."B01001_006" + acs."B01001_007" + acs."B01001_030" + acs."B01001_031" AS age_15_19,
+    acs."B01001_008" + acs."B01001_009" + acs."B01001_010" + acs."B01001_032" + acs."B01001_033" + acs."B01001_034" AS age_20_24,
+    acs."B01001_011" + acs."B01001_012" + acs."B01001_035" + acs."B01001_036" AS age_25_34,
+    acs."B01001_013" + acs."B01001_014" + acs."B01001_037" + acs."B01001_038" AS age_35_44,
+    acs."B01001_015" + acs."B01001_016" + acs."B01001_039" + acs."B01001_040" AS age_45_54,
+    acs."B01001_017" + acs."B01001_018" + acs."B01001_019" + acs."B01001_041" + acs."B01001_042" + acs."B01001_043" AS age_55_64,
+    acs."B01001_020" + acs."B01001_021" + acs."B01001_022" + acs."B01001_044" + acs."B01001_045" + acs."B01001_046" AS age_65_74,
+    acs."B01001_023" + acs."B01001_024" + acs."B01001_047" + acs."B01001_048" AS age_75_84,
+    acs."B01001_025" + acs."B01001_049" AS age_85_up,
+    acs."B25002_001" AS hse_units,
+    acs."B25002_003" AS vacant,
+    acs."B25014_008" AS renter_occ,
+    acs."B25014_005" + acs."B25014_006" + acs."B25014_007" + acs."B25014_011" + acs."B25014_012" + acs."B25014_013" AS crowded,
+    acs."B25024_002" + acs."B25024_003" + acs."B25024_004" AS sfr,
+    acs."B25024_007" + acs."B25024_008" + acs."B25024_009" AS units_10,
+    acs."B25024_010" AS mh,
+    acs."B25034_006" + acs."B25034_007" + acs."B25034_008" + acs."B25034_009" + acs."B25034_010" AS older,
     pcl.apts_n AS apt_parcels,
     pcl.mr_n AS mr_parcels,
-    acs."B19013_001E" AS inc_hh,
+    acs."B19013_001" AS inc_hh,
     svi.r_pl_themes AS svi,
     acs."B12001_001" - (acs."B12001_003" + acs."B12001_012") AS married,
     acs."B23025_005" AS unemployed,
@@ -128,11 +128,11 @@ CREATE MATERIALIZED VIEW nist.med_risk_fires AS
      LEFT JOIN d ON tr.fc_dept_id = d.fd_id AND tr.year::double precision = d.year
      LEFT JOIN nist.svi2010 svi ON tr.tr10_fid = ('14000US'::text || lpad(svi.fips::text, 11, '0'))
      LEFT JOIN nist.acs_est_new acs ON tr.tr10_fid = acs.geoid AND
-        CASE
-            WHEN tr.year < 2008 THEN 2008
-            WHEN tr.year > 2013 THEN 2013
-            ELSE tr.year
-        END::double precision = (acs.year - 2::double precision)
+       CASE
+         WHEN tr.year < 2008 THEN 2008
+         WHEN tr.year > (select max(year) from nist.acs_est_new) - 2 THEN (select max(year) from nist.acs_est_new) - 2
+         ELSE tr.year
+       END::double precision = (acs.year - 2::double precision)
      LEFT JOIN nist.sins sm ON tr.state::text = sm.postal_code AND sm.year = 2010
      LEFT JOIN nist.sins_county sc ON "substring"(tr.tr10_fid, 8, 5) = sc.fips
      LEFT JOIN c ON tr.tr10_fid = c.geoid AND tr.year::double precision = c.year
