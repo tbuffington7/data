81eb11a7eee8823d845c2aebbf5adb2dd59271fe
Joe Meilinger <joe@prominentedge.com>
2018-09-13 07:33:19 -0500

    Allow for department's provided NFIRS to override NFPA NFIRS

--- sources/nfirs/scripts/materialized_views/public.joint_fireincident.sql
+++ sources/nfirs/scripts/materialized_views/public.joint_fireincident.sql
@@ -1,3 +1,8 @@
-CREATE MATERIALIZED VIEW IF NOT EXISTS public.joint_fireincident AS
-    SELECT * FROM public.fireincident UNION ALL
-    SELECT * FROM department_nfirs.fireincident;
+CREATE MATERIALIZED VIEW joint_fireincident AS
+SELECT * FROM public.fireincident fi
+  WHERE (state, fdid, inc_date, inc_no, exp_no)
+    NOT IN (select state, fdid, inc_date, inc_no, exp_no FROM departme
+UNION ALL
+SELECT * FROM department_nfirs.fireincident dfi;
+    
+CREATE UNIQUE INDEX ON joint_fireincident (state, fdid, inc_date, inc_no, exp_no);
